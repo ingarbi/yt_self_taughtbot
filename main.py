25264bc7ac9de38592259b8f70134f584087c79c
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-06 22:56:03 +0300

    Finished

---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+from difflib import get_close_matches
+
+
+def load_knowledge_base(file_path: str) -> dict:
+    with open(file_path, "r") as file:
+        data: dict = json.load(file)
+    return data
+
+
+def find_best_match(user_question: str, questions: list[str]) -> str or None:
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+
+def get_answer_for_question(question: str, knowledge_base: dict) -> str or None:
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
+        return None
+
+
+def save_knowledge_base(file_path: str, data: dict):
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=2)
+
+
+def chat_bot():
+    knowledge_base: dict = load_knowledge_base("knowledge_base.json")
+
+    while True:
+        user_input: str = input("You: ")
+
+        if user_input.lower() == "quit":
+            break
+
+        best_match: str or None = find_best_match(
+            user_input, [q["question"] for q in knowledge_base["questions"]]
+        )
+
+        if best_match:
+            answer: str = get_answer_for_question(best_match, knowledge_base)
+            print(f"Bot: {answer}")
+        else:
+            print("I dont know the answer. Please teach me")
+            new_answer: str = input("Write the answer or 'skip' to skip: ")
+
+            if new_answer.lower() != "skip":
+                knowledge_base["questions"].append(
+                    {"question": user_input, "answer": new_answer}
+                )
+
+                save_knowledge_base("knowledge_base.json", knowledge_base)
+
+                print("Bot: THX! I have learnt a new response")
+
+
+if __name__ == "__main__":
+    chat_bot()
